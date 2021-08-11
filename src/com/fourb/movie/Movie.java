20543cdab32b1ca1bbf46d72470c7f4b5b828404
gyugyu90 <kh2000park@gmail.com>
2021-08-11 21:43:50 +0900

    ReservationAgency 결합도 개선

--- src/com/fourb/movie/Movie.java
+++ src/com/fourb/movie/Movie.java
@@ -1,6 +1,7 @@
 package com.fourb.movie;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Movie {
@@ -22,6 +23,52 @@ public class Movie {
         this.discountPolicy = discountPolicy;
     }
 
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public Money calculateAmountDiscountedFee() {
+        if (movieType != MovieType.AMOUNT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(discountAmount);
+    }
+
+    public Money calculatePercentDiscountedFee() {
+        if (movieType != MovieType.PERCENT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(fee.times(discountPercent));
+    }
+
+    public Money calculateNoneDiscountedFee() {
+        if (movieType != MovieType.NONE_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee;
+    }
+
+    public boolean isDiscountable(LocalDateTime whenScreened, int sequence) {
+        for (DiscountCondition condition : discountConditions) {
+            if (condition.getType() == DiscountConditionType.PERIOD) {
+                if (condition.isDiscountable(whenScreened.getDayOfWeek(), whenScreened.toLocalTime())) {
+                    return true;
+                }
+            } else {
+                if (condition.isDiscountable(sequence)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
     public Money getFee() {
         return fee;
     }
@@ -41,10 +88,6 @@ public class Movie {
 
     public void setDiscountConditions(List<DiscountCondition> discountConditions) {
         this.discountConditions = discountConditions;
-    }
-
-    public MovieType getMovieType() {
-        return movieType;
     }
 
     public void setMovieType(MovieType movieType) {
