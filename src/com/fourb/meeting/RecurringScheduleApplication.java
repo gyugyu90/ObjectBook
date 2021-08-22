f7128bafa3b48f4d6b5b80a9a2d9f140ea6d871b
gyugyu90 <kh2000park@gmail.com>
2021-08-22 14:03:57 +0900

    command query segregation

--- src/com/fourb/meeting/RecurringScheduleApplication.java
+++ src/com/fourb/meeting/RecurringScheduleApplication.java
@@ -5,12 +5,15 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-public class RecurringScheduleTest {
+public class RecurringScheduleApplication {
+
     public static void main(String[] args) {
         RecurringSchedule schedule = new RecurringSchedule("meeting", DayOfWeek.WEDNESDAY, LocalTime.of(10, 30), Duration.ofMinutes(30));
         Event meeting = new Event("meeting", LocalDateTime.of(2019, 5, 9, 10, 30), Duration.ofMinutes(30));
 
-        assert meeting.isSatisfied(schedule) == false;
-        assert meeting.isSatisfied(schedule) == true;
+        if (meeting.isSatisfied(schedule)) {
+            meeting.reschedule(schedule);
+        }
     }
+
 }
