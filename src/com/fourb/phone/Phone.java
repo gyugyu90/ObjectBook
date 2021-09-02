6f95398e4ec4d7a963ae5794be91690689ddf0f6
gyugyu90 <kh2000park@gmail.com>
2021-09-03 08:34:10 +0900

    생성 로직의 변경

--- src/com/fourb/phone/Phone.java
+++ src/com/fourb/phone/Phone.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 public abstract class Phone {
 
+    private double taxRate;
     private List<Call> calls = new ArrayList<>();
 
+    public Phone(double taxRate) {
+        this.taxRate = taxRate;
+    }
 
     public Money calculateFee() {
         Money result = Money.ZERO;
@@ -17,7 +21,7 @@ public abstract class Phone {
             result = result.plus(calculateCallFee(call));
         }
 
-        return result;
+        return result.plus(result.times(taxRate));
     }
 
     protected abstract Money calculateCallFee(Call call);
