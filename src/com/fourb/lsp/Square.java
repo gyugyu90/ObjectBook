6306d9f566ca42cb809fc889216343edcacd9944
gyugyu90 <kh2000park@gmail.com>
2021-09-12 16:18:35 +0900

    13. 서브클래싱과 서브타이핑

---
+++ src/com/fourb/lsp/Square.java
@@ -0,0 +1,21 @@
+package com.fourb.lsp;
+
+public class Square extends Rectangle {
+
+    public Square(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void setWidth(int width) {
+        super.setWidth(width);
+        super.setHeight(width);
+    }
+
+    @Override
+    public void setHeight(int height) {
+        super.setHeight(height);
+        super.setHeight(height);
+    }
+}
+
